7af908d0fec23668ea96d84f2239d7838b719baf
Ignacio-CH <ichurataalzu@uade.edu.ar>
2026-09-01 14:25:23 -0300

    Add validar.py

---
+++ validar.py
@@ -0,0 +1,8 @@
+"""
+validar.py
+----------------
+Funciones de validación mediante expresiones regulares.
+Formatos de patente admitidos:
+    - Formato viejo:     ABC123   (3 letras + 3 números)
+    - Formato Mercosur:  AB123CD  (2 letras + 3 números + 2 letras)
+"""
