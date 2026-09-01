b41dafde19593bbb41d6af2269b08d1209e54c24
MatiasLusso <mlussoro@uade.edu.ar>
2026-09-01 14:40:28 -0300

    Expresiones regulares no regularizadas y sin expresar

---
+++ IngresoMat.py
@@ -0,0 +1,17 @@
+'''patentes = []
+
+pat = int(input("Ingrese la patente del vehiculo: "))
+patentes.append(pat)
+
+print("Patentes ingresadas: ", patentes)
+
+color = []
+
+col = int(input("Ingrese el color del vehiculo: "))
+color.append(col)
+
+print("Colores ingresados: ", color)'''
+
+
+
+
