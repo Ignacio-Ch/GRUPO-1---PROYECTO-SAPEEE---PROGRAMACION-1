56b983d52026c7ebf30b3a831ed6ad0558b5c1be
MatiasLusso <mlussoro@uade.edu.ar>
2026-09-06 15:43:09 -0300

    Matrizactualizacion

--- Matriz.py
+++ Matriz.py
@@ -7,29 +7,50 @@ def matrizestacionamiento(n, m):
         matriz.append(fila_nueva)
     return matriz
 
+def numeropositivo(x):
+    while x <= 0:
+        x = int(input("Ingrese un valor positivo: "))
+    return x
+
 def ingresotamañomatriz():
     print("Ingrese el tamaño de la matriz (mxn)")
-    x = int(input("Ingrese el parametro m:"))
+    x = int(input("Ingrese el parametro m: "))
     x = numeropositivo(x)
-    y = int(input("Ingrese el parametro n:"))
+    y = int(input("Ingrese el parametro n: "))
     y = numeropositivo(y)
     return x, y
 
-def numeropositivo(x):
-    while x <=0:
-        x = int(input("Ingrese un valor positivo"))
-    return x
+def buscarespaciodisponible(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == 'libre':
+                return i, j
 
+def lugareslibres(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == 'libre':
+                return True
+    return False              
 
-
+def guardarpatentes(matriz):
+    patente = input("Ingrese la patente o 'listo' para terminar: ")
+    while patente != 'listo':
+        if lugareslibres(matriz):
+            x, y = buscarespaciodisponible(matriz)
+            matriz[x][y] = patente
+        else:
+            print("No hay lugares disponibles")
+        patente = input("Ingrese la patente o 'listo' para terminar: ")
+    return matriz
 
 def main():
     n, m = ingresotamañomatriz()
     mat = matrizestacionamiento(n, m)
     for i in range(len(mat)):
         print(mat[i])
-
-
-
+    mat = guardarpatentes(mat)
+    for i in range(len(mat)):
+        print(mat[i])
 
 main()
